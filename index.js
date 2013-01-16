ba32b0116fa8094d8c048c8d0472a0c2db63f018
Thomas <thomas@ordinateur-fixe.(none)>
2013-01-16 01:50:56 +0100

    Affichage client via angularJS

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-oConnections = JSON.parse(localStorage['connectionHistory']);
-
-oConnections.forEach(function(y) {
-						oTable = $( '#table tbody');
-						oTable.html( oTable.html() + "<tr>" + y.date + "</tr>");
-						
-					}); 
+function listConnection( $scope) {	  	
+  $scope.connections = JSON.parse(localStorage['connectionHistory']);
+}
